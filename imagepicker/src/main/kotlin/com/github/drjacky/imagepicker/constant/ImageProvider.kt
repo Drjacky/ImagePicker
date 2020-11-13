b39abca22bc0e9f8b0dd6d81fcb6145e0497dbe4
Dr.jacky <dr.jacky.2005@gmail.com>
2020-11-13 11:36:48 +0000

    Rename necessary package name

---
+++ imagepicker/src/main/kotlin/com/github/drjacky/imagepicker/constant/ImageProvider.kt
@@ -0,0 +1,15 @@
+package com.github.drjacky.imagepicker.constant
+
+/**
+ * Define Image Provider
+ *
+ * @author Dhaval Patel
+ * @version 1.0
+ * @since 04 January 2019
+ */
+enum class ImageProvider {
+    GALLERY,
+    CAMERA,
+    FRONT_CAMERA,
+    BOTH
+}
