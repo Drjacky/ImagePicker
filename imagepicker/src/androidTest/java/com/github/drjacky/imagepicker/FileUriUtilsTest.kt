8094bf2197542da959189c118b10049eee55a8b4
Drjacky <dr.jacky.2005@gmail.com>
2022-12-19 09:14:04 +0000

    Update internal dependencies

---
+++ imagepicker/src/androidTest/java/com/github/drjacky/imagepicker/FileUriUtilsTest.kt
@@ -0,0 +1,22 @@
+package com.github.drjacky.imagepicker
+
+import android.net.Uri
+import androidx.test.platform.app.InstrumentationRegistry
+import com.github.drjacky.imagepicker.util.FileUriUtils
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class FileUriUtilsTests {
+
+    @Test
+    fun getImageExtension_AndroidPhotoPickerPath_returnsJpg() {
+        val uri =
+            Uri.parse("content://media/picker/0/com.android.providers.media.photopicker/media/1000000033")
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        val extension = FileUriUtils.getImageExtension(appContext, uri)
+
+        assertEquals(".jpg", extension)
+    }
+
+}
